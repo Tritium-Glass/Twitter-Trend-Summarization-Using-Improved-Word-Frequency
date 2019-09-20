1d445ba66082e8e8926f406cc2ca006ec3b0657c
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-09-21 00:30:06 +0530

    Webscraping for Aljazeera completed

--- web_scraping_prototype.py
+++ web_scraping_prototype.py
@@ -2,16 +2,18 @@
 
 import requests
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import os
 from time import sleep
 from text_sum import make_summary
 from bs4 import BeautifulSoup
+import bs4
 
-def main():
+def bbc_search(phrase):
 	#while True:
 	#search = input("Enter an item to search: ")
-	search = "hello there"
+	search = phrase
 	search = '+'.join(search.split())
 	url = "https://www.bbc.co.uk/search?q="+search+"&filter=news"
 
@@ -31,7 +33,7 @@ def main():
 	print(links)
 	get_content(links[2])
 
-def get_content(link):
+def bbc_webpage_to_text(link):
 	print(link)
 	page = requests.get(link)
 	#print(page.text[:1000])
@@ -59,5 +61,68 @@ def get_content(link):
 	print(text,'\n\n\n')
 	#make_summary(text)
 
+def start_browser():
+
+	dir = os.path.dirname(__file__)
+	chrome_driver_path = dir + "/chromedriver.exe"
+	chrome_options = Options()
+	#"https://www.aljazeera.com/Search/?q="+searchchrome_options.add_argument("--headless")
+	#chrome_options.binary_location = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+	# create a new Chrome session
+
+	return webdriver.Chrome(options=chrome_options)
+
+def cnn_search(phrase):
+
+	driver = start_browser()
+
+	search = phrase
+	search = '+'.join(search.split())
+
+	driver.get("https://www.aljazeera.com/Search/?q="+search)
+	sleep(20)
+	articles = driver.find_elements_by_xpath("//div[@class='row topics-sec-item   ']")
+
+	article_links = []
+
+	for article in articles:
+		# print(article.text)
+		exact_article = article.find_element_by_xpath("div[@class='col-sm-7 topics-sec-item-cont']")
+		article_link = exact_article.find_element_by_xpath("a")
+		# print(exact_article.text)
+		# print(article_link.text)
+		if phrase in article_link.text.lower():
+			article_links.append(article_link.get_attribute('href'))
+
+	driver.close()
+
+	for article_link in article_links:
+		cnn_webpage_to_text(article_link)
+
+	# cnn_webpage_to_text(article_links[0])
+
+def cnn_webpage_to_text(link):
+
+	page = requests.get(link)
+	#print(page.text[:1000])
+	soup = BeautifulSoup(page.text, 'html5lib')
+
+	body_content = soup.find('div',attrs={'class':"article-p-wrapper"})
+
+	sentence_list = body_content.find_all('p')
+
+	text = []
+	for sentence in sentence_list:
+		if isinstance(sentence.contents[0],bs4.element.NavigableString):
+			text.append(sentence.contents[0])
+
+	# print(text)
+	text= ''.join(text)
+
+	try:
+		print(text,'\n')
+	except Exception as e:
+		pass
+
 if __name__ == '__main__':
-	main()
+	cnn_search('kashmir')
