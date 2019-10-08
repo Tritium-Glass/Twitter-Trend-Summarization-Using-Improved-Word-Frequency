a77088b5c84e9e5501ebe191233ed0fa0e769fb3
GabrielDaKing <34449228+GabrielDaKing@users.noreply.github.com>
2019-10-09 02:54:41 +0530

    Minor Fix

--- get_f1_score.py
+++ get_f1_score.py
@@ -1,18 +1,18 @@
 import web_scraping_prototype as WSP
 import auto_summarization as autosum
-import tf_idf
-# import word_freq
+from tf_idf import tf_idf_summarise
+from word_freq import word_freq_summarize
 import summary_evaluation as sumeval
 
 def main():
-    articles = WSP.aljazeera_search('chandrayaan')
-    print(articles)
+    articles = WSP.aljazeera_search('iphone')
+    print(len(articles))
     for article in articles:
         ref_sum = autosum.get_summary(article)
-        # sys_sum = word_freq.word_freq_summarize(article)
-        sys_sum = tf_idf.tf_idf_summarise(article)
+        sys_sum = word_freq_summarize(article)
+        # sys_sum = tf_idf_summarise(article)
         matching_bigrams, precision, recall, f_measure = sumeval.rouge(ref_sum, sys_sum)
-        print('Ref Sum:',ref_sum,'\nSys Sum:',sys_sum)
+        #print('Ref Sum:',ref_sum,'\nSys Sum:',sys_sum)
         print('\nB:',matching_bigrams,'\tP:',precision,'\tR',recall,'\tF',f_measure)
 
 if __name__ == "__main__":
