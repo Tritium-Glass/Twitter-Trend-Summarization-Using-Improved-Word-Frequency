c4f1a596d91e2d41f0445faca47f5079352d81f5
SamDaQueen <samreenreyaz@outlook.com>
2019-10-11 00:01:03 +0530

    Exception handling in get_f1_score for TextRank

--- get_f1_score.py
+++ get_f1_score.py
@@ -2,14 +2,19 @@ import web_scraping_prototype as WSP
 import auto_summarization as autosum
 from tf_idf import tf_idf_summarise
 from word_freq import word_freq_summarize
+from gensim.summarization.summarizer import summarize
 import summary_evaluation as sumeval
 
 def main():
-    articles = WSP.aljazeera_search('iphone')
+    articles = WSP.aljazeera_search('chandrayaan')
     print(len(articles))
     for article in articles:
-        ref_sum = autosum.get_summary(article)
-        sys_sum = word_freq_summarize(article)
+        try:
+            ref_sum = autosum.get_summary(article)
+            sys_sum = summarize(article)
+        except:
+            continue
+        # sys_sum = word_freq_summarize(article)
         # sys_sum = tf_idf_summarise(article)
         matching_bigrams, precision, recall, f_measure = sumeval.rouge(ref_sum, sys_sum)
         #print('Ref Sum:',ref_sum,'\nSys Sum:',sys_sum)
