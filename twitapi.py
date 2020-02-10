05c9061cc2890eb6c9c20ab05cf7c72f47a8e535
Dishant100 <37150427+Dishant100@users.noreply.github.com>
2020-02-10 23:49:34 +0530

    Update twitapi.py
    abcd

--- twitapi.py
+++ twitapi.py
@@ -1,4 +1,6 @@
-import twitter,json,re
+import twitter,json,re,nltk
+#nltk.download('words')
+from langdetect import detect
 
 consumer_key = 'S8gMgBFhScZ1PEQJqYmoDYcL9'
 consumer_secret = 'SupHG06pkr15zhXyFA9W66fRejODbc7bKNnRILcrQoouG2cgbh'
@@ -19,12 +21,20 @@ Mumbai = 2295411
 # world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 # us_trends = twitter_api.trends.place(_id=US_WOE_ID)
 trends = twitter_api.trends.place(_id=Mumbai)
+#words = set(nltk.corpus.words.words())
 
 #print (mum_trends)
 # print (json.dumps(trends, indent=1))
 for trend in trends[0]["trends"]:
     try:
     	t = trend["name"].strip("#")
-    	print(re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', t).lower())
+    	tt = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', t).lower()
+    	l = len(tt)
+    	b = tt[l-1]
+    	a = tt[0]
+    	if(ord(a)<=122 and ord(a)>=97):
+    		if(ord(b)<=122 and ord(b)>=97):
+    			print(tt)
     except Exception as e:
         pass
+
